799b3d204e5efa1aa5e7fa0d69a9b2cfbb010d47
BaraAksayeth25 <mbara4321@gmail.com>
2020-09-20 13:14:52 +0700

    Add Helmet Package, Cors Handler, Error Handler

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const helmet = require("helmet");
 
 // DB Connection
 const { db_url, options } = require("./config/db");
@@ -14,12 +15,29 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Set Middlewares
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // Parser every request body to json
+app.use(helmet()); // Manage http headers respons
+app.disable("x-powered-by");
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Accept, Authorization, Content-Type"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH");
+  next();
+});
 
 // Set Routes
 app.use("/api/product", productRoute);
 app.use("/api/category", categoryRoute);
 app.use("/api/user", userRoute);
+
+// Error Handler
+app.use((err, req, res, next) => {
+  res.status(err.code || 500);
+  res.json({ message: err.message || "An Unknown Error!" });
+});
 
 // Run Database and server
 mongoose
