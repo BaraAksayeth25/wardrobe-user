07a1d3d2837890b630513f67f24cbb362b4b1d0d
BaraAksayeth25 <mbara4321@gmail.com>
2020-10-07 19:24:04 +0700

    Add middlewares for authentication

---
+++ middlewares/authenticate.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const HttpError = require("../models/http-error");
+
+module.exports = (req, res, next) => {
+  if (req.method === "OPTIONS") return next();
+
+  const token = req.headers.authorizations.split(" ")[1];
+
+  if (!token) return next(new HttpError("Access Denied", 403));
+
+  try {
+    const decodeToken = jwt.verify(token, process.env.JWT_KEY_ACCESS);
+
+    req.userData = decodeToken;
+
+    next();
+  } catch (err) {
+    return next(new HttpError("Error In Verify Token", 500));
+  }
+};
