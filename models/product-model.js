dba53bceeff2c2187e57e605b9f4082198dcb3d2
BaraAksayeth25 <mbara4321@gmail.com>
2020-11-05 00:21:14 +0700

    chore(model): add timestamps

--- models/product-model.js
+++ models/product-model.js
@@ -1,41 +1,44 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    maxlength: 255,
-    required: true,
-  },
-  category: {
-    type: mongoose.Types.ObjectId,
-    ref: "category",
-    required: true,
-  },
-  description: {
-    type: String,
-  },
-  stocks: [
-    {
-      size: {
-        type: String,
-      },
-      stock: {
-        type: Number,
-        min: 0,
-        required: true,
-      },
-      _id: false,
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      maxlength: 255,
+      required: true,
     },
-  ],
-  price: {
-    type: Number,
-    min: 0,
-    required: true,
+    category: {
+      type: mongoose.Types.ObjectId,
+      ref: "category",
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    stocks: [
+      {
+        size: {
+          type: String,
+        },
+        stock: {
+          type: Number,
+          min: 0,
+          required: true,
+        },
+        _id: false,
+      },
+    ],
+    price: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
+    image: {
+      type: String,
+      default: process.env.DEFAULT_IMAGE_PRODUCT,
+    },
   },
-  image: {
-    type: String,
-    default: process.env.DEFAULT_IMAGE_PRODUCT,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("product", productSchema);
