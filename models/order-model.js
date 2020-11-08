6242e5ea05a6783cd2abc4bafc9ece2135d4e884
BaraAksayeth25 <mbara4321@gmail.com>
2020-11-08 13:42:34 +0700

    chore(model): add status, bookingCode on order schema

--- models/order-model.js
+++ models/order-model.js
@@ -1,43 +1,55 @@
 const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    maxLength: 255,
-    required: true,
-  },
-  total_price: {
-    type: Number,
-    min: 0,
-    required: true,
-  },
-  products: [
-    {
-      _id: {
-        type: mongoose.Types.ObjectId,
-        required: true,
-        ref: "product",
-      },
-      size: {
-        type: String,
-      },
-      stock: {
-        type: Number,
-        min: 1,
-      },
+const orderSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      maxLength: 255,
+      required: true,
     },
-  ],
-  date: {
-    type: Date,
-    default: () => +new Date() + 1000,
+    bookingCode: {
+      type: String,
+      maxLength: 255,
+    },
+    totalPrice: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
+    products: [
+      {
+        _id: {
+          type: mongoose.Types.ObjectId,
+          required: true,
+          ref: "product",
+        },
+        size: {
+          type: String,
+        },
+        stock: {
+          type: Number,
+          min: 1,
+        },
+      },
+    ],
+    date: {
+      type: Date,
+    },
+    days: {
+      type: Number,
+      min: 0,
+    },
+    overDue: {
+      type: Date,
+    },
+    status: {
+      type: String,
+      default: "On Going",
+    },
   },
-  day: {
-    type: Number,
-    min: 0,
-  },
-  overDue: {
-    type: Date,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("order", orderSchema);
