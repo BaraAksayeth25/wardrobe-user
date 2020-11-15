65db8ac95dfacb47f16a73ec5e31d81d5de42112
BaraAksayeth25 <mbara4321@gmail.com>
2020-11-15 10:53:18 +0700

    feat(api): create crud products

---
+++ controllers/admin/product-controller.js
@@ -0,0 +1,113 @@
+const path = require("path");
+const fs = require("fs");
+const { validationResult } = require("express-validator");
+
+const ProductModel = require("../../models/product-model");
+const HttpError = require("../../models/http-error");
+
+const createProduct = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty) {
+    return next(
+      new HttpError(`${errors.errors[0].msg} in ${errors.errors[0].param}`, 400)
+    );
+  }
+
+  const { name, description, category, price, stocks } = req.body;
+  console.log(stocks);
+  try {
+    await ProductModel.create({
+      name,
+      category,
+      description,
+      stocks,
+      price,
+      image: req.file && req.file.filename,
+    });
+  } catch (err) {
+    return next(new HttpError(err.message, 500));
+  }
+  res.json({ message: "OK" });
+};
+
+const updateProduct = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty) {
+    return next(
+      new HttpError(`${errors.errors[0].msg} in ${errors.errors[0].param}`, 400)
+    );
+  }
+
+  const id = req.params.id;
+  const { name, description, category, price, stocks } = req.body;
+
+  let product;
+  try {
+    product = await ProductModel.findById(id);
+  } catch (err) {
+    return next(new HttpError(err.message, 500));
+  }
+  if (!product) {
+    return next(new HttpError("Product Not Found", 404));
+  }
+  if (req.file && product.image !== process.env.DEFAULT_IMAGE_PRODUCT) {
+    try {
+      fs.unlinkSync(path.resolve("uploads", "images", product.image));
+    } catch (err) {
+      return next(new HttpError(err, 500));
+    }
+  }
+
+  product.name = name || product.name;
+  product.description = description || product.description;
+  product.category = category || product.category;
+  product.price = price || product.price;
+  product.stocks = stocks || product.stocks;
+
+  if (req.file) {
+    product.image = req.file.filename;
+  }
+
+  try {
+    await product.save();
+  } catch (err) {
+    return next(new HttpError(err.message, 500));
+  }
+
+  res.json({ message: "OK" });
+};
+
+const deleteProduct = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty) {
+    return next(
+      new HttpError(`${errors.errors[0].msg} in ${errors.errors[0].param}`)
+    );
+  }
+
+  const id = req.params.id;
+
+  let product;
+  try {
+    product = await ProductModel.findByIdAndDelete(id);
+  } catch (err) {
+    return next(new HttpError(err.message, 500));
+  }
+  if (!product) {
+    return next(new HttpError("Product not found", 404));
+  }
+  if (product.image !== process.env.DEFAULT_IMAGE_PRODUCT) {
+    try {
+      fs.unlinkSync(path.resolve("uploads", "images", product.image));
+    } catch (err) {
+      return next(new HttpError(err, 500));
+    }
+  }
+  res.json({ message: "OK" });
+};
+
+module.exports = {
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
--- routes/product-route.js
+++ routes/product-route.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const { check } = require("express-validator");
+
+const adminAuthenticate = require("../middlewares/authenticate-admin");
+const imageUpload = require("../middlewares/image-upload");
+
 const route = express.Router();
 
 const {
@@ -8,8 +13,13 @@ const {
   getProductByCategory,
   getTopProduct,
   getProductByName,
-  createProduct,
 } = require("../controllers/user/product-controller");
+
+const {
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/admin/product-controller");
 
 route.get("/id/:id", getProductById);
 route.get("/pages/:pages", getAllProduct);
@@ -17,6 +27,33 @@ route.get("/ct/:ct/pages/:pages", getProductByCategory);
 route.get("/name/:name/pages/:pages", getProductByName);
 route.get("/latest", getLatestProduct);
 route.get("/top", getTopProduct);
-route.post("/add", createProduct);
+
+route.use(adminAuthenticate);
+
+route.post(
+  "/add",
+  [
+    check("name").notEmpty().isLength({ min: 1 }),
+    check("description").isString().notEmpty(),
+    check("price").isInt({ min: 0 }),
+    check("category").isMongoId(),
+  ],
+  imageUpload.single("image"),
+  createProduct
+);
+
+route.patch(
+  "/edit/:id",
+  [
+    check("name").optional().notEmpty().isLength({ min: 1 }),
+    check("description").optional().isString().notEmpty(),
+    check("price").optional().isInt({ min: 0 }),
+    check("category").optional().isMongoId(),
+  ],
+  imageUpload.single("image"),
+  updateProduct
+);
+
+route.delete("/delete/:id", check("id").isMongoId(), deleteProduct);
 
 module.exports = route;
