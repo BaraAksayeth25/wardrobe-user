85eb289dcf5cda723a81f65d0423989b77e53380
BaraAksayeth25 <mbara4321@gmail.com>
2020-11-30 14:22:52 +0700

    feat(helper): create count beetwen date function

--- helpers/date.js
+++ helpers/date.js
@@ -1,5 +1,11 @@
-module.exports = (date, days) => {
+module.exports.addDays = (date, days) => {
   const copy = new Date(+date);
   copy.setDate(date.getDate() + days);
   return copy;
+};
+
+module.exports.countDays = (overDue, date) => {
+  const diffTime = date.getTime() - overDue.getTime();
+  const dateInDiff = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+  return dateInDiff < 0 ? 0 : dateInDiff;
 };
