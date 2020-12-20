bc4359eabd37efee1a11120b6313db6d4f0563a1
BaraAksayeth25 <mbara4321@gmail.com>
2020-12-20 17:40:16 +0700

    fix(server): CORS

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 const fs = require("fs");
+const cors = require("cors");
 const path = require("path");
 const HttpError = require("./models/http-error");
 const CronJob = require("cron").CronJob;
@@ -28,15 +29,7 @@ app.use(bodyParser.json()); // Parser every request body to json
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
 app.use(helmet()); // Manage http headers respons
 app.disable("x-powered-by");
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Accept, Authorization, Content-Type"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH");
-  next();
-});
+app.use(cors());
 
 // Set Routes
 app.use("/api/products", productRoute);
